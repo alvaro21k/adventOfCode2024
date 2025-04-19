42f5373afc1dbee0e45a79cd37ffd063eb395796
Alvaro Nicosia <alvaro21k@gmail.com>
2025-04-19 16:25:39 +0200

    implemented day 1 part 1

--- adventOfCode2024/src/day_mains/dayone.h
+++ adventOfCode2024/src/day_mains/dayone.h
@@ -4,24 +4,26 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
 void read_file();
 
 void read_file() {
-	std::ifstream inputFile("dayone.txt");
-	std::deque<std::string> first_column;
-	std::deque<std::string> second_column;
+	std::ifstream inputFile("./inputs/day1.txt", std::ifstream::in);
+	std::deque<int> first_column;
+	std::deque<int> second_column;
 	std::string control_str;
 	std::string str_one;
 	std::string str_two;
+
+	int final_result = 0;
 	
 	if (!inputFile) {
 		std::cout << "Could not read the file" << std::endl;
 	}
 	else {
-		while (inputFile) {
-			std::cout << 'test' << std::endl;
-			std::getline(inputFile, control_str);
+		while (std::getline(inputFile, control_str)) {
 			bool is_second = false;
 			for (char c : control_str) {
 				if (c != ' ' && !is_second) {
@@ -34,12 +36,26 @@ void read_file() {
 					is_second = true;
 				}
 			}
-			//first_column.emplace_back(std::stoi(str_one));
-			//second_column.emplace_back(std::stoi(str_two));
-			std::cout << str_one << ' ' << str_two << std::endl;
+			first_column.emplace_back(std::stoi(str_one));
+			second_column.emplace_back(std::stoi(str_two));
 			str_one.clear();
 			str_two.clear();
 		}
 	}
 	inputFile.close();
+
+	std::sort(first_column.begin(), first_column.end());
+	std::sort(second_column.begin(), second_column.end());
+
+	std::cout << first_column.size() << std::endl;
+	std::cout << second_column.size() << std::endl;
+	for (int i = 0; i < first_column.size(); i++)
+	{
+		std::cout << i << ' ' << first_column[i] << ' ' << second_column[i] << ' ' << abs(first_column[i] - second_column[i]) << std::endl;
+
+		int loop_result = abs(first_column[i] - second_column[i]);
+		final_result += loop_result;
+	}
+	std::cout << "The final result is: " << final_result << std::endl;
+	
 }
--- adventOfCode2024/src/main.cpp
+++ adventOfCode2024/src/main.cpp
@@ -1,6 +1,12 @@
 #include "./day_mains/dayone.h"
+#include <direct.h>
 
 int main(){
+    char buffer[256];
+    char* val = _getcwd(buffer, sizeof(buffer));
+    if (val) {
+        std::cout << buffer << std::endl;
+    }
 	//first day
 	read_file();
 
